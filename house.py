6f5cbe9f0dd32f627f73a38f44f53bebdc4b4052
menyuu <101458830+menyuu@users.noreply.github.com>
2022-11-17 23:44:29 +0900

    アルゴリズムの学習

--- house.py
+++ house.py
@@ -3,6 +3,7 @@ orange = int(input())
 print(apple + orange)
 
 
+
 n = int(input())
 a = [int(i) for i in input().split()]
 sum = 0
@@ -10,3 +11,17 @@ sum = 0
 for i in range(n):
     sum += a[i]
 print(sum)
+
+
+
+def trance_binary():
+    n = int(input())
+    ans = ''
+    while n >= 1:
+        if n % 2 == 0:
+            ans = '0' + ans
+        if n % 2 == 1:
+            ans = '1' + ans
+        n = n // 2
+    return ans
+print(trance_binary())
