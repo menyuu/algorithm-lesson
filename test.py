df4f03479f5565d6365d156664f3c840a83d33d9
menyuu <101458830+menyuu@users.noreply.github.com>
2022-11-19 23:56:26 +0900

    アルゴリズムの学習

--- test.py
+++ test.py
@@ -117,3 +117,23 @@ for i in range(n):
 ans.sort()
 for i in ans:
     print(i)
+
+
+#########################################################
+
+
+p = []
+ans_p = 0
+ans_num = 0
+
+for i in range(4):
+    p += input().split()
+    print(p)
+
+for i in range(10):
+    if p[i] == "1":
+        ans_num += 1
+        ans_p = 10 - i
+
+print(ans_p)
+print(ans_num)
