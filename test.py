aa665a20341a06723914dfd9cafaf6f6bb794d21
menyuu <101458830+menyuu@users.noreply.github.com>
2022-12-26 02:08:25 +0900

    アルゴリズムの学習

--- test.py
+++ test.py
@@ -249,3 +249,21 @@ for _ in range(k):
 
 for i, j in d.items():
     print(i, d[i][1])
+
+
+#########################################################
+
+
+v = input().split()
+N = int(v[0])
+K = float(v[1])
+
+total = 0
+for _ in range(N):
+    x = float(input())
+    total += round(x * 10)
+
+ans = int(total / round(K * 10))
+if total % round(K * 10) != 0:
+    ans += 1
+print(ans)
